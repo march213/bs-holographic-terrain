e28d42f1794f2e920c9e5241de3c2f97a8f693e8
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-07-25 11:36:59 -0400

    add alpha step to fragment shader

--- src/shaders/terrain/fragment.glsl
+++ src/shaders/terrain/fragment.glsl
@@ -1,9 +1,8 @@
 varying float vElevation;
 
 void main() {
-  // float elevation = vElevation + 0.5;
-
   float alpha = mod(vElevation * 30.0, 1.0);
+  alpha = step(0.8, alpha);
 
   gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
 }
