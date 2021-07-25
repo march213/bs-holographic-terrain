ee8264345117d07b7fbdeefbd352ee4be0a875d7
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-07-25 14:26:20 -0400

    add effect composer, other tweaks

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js'
 import guify from 'guify'
 
 import terrainVertex from './shaders/terrain/vertex.glsl'
@@ -30,12 +33,14 @@ const debug = new guify({
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
 }
 
 window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
   // Update camera
   camera.aspect = sizes.width / sizes.height
@@ -43,7 +48,15 @@ window.addEventListener('resize', () => {
 
   // Update renderer
   renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  renderer.setPixelRatio(sizes.pixelRatio)
+
+  // Update effect composer
+  effectComposer.setSize(sizes.width, sizes.height)
+  effectComposer.setPixelRatio(sizes.pixelRatio)
+
+  // Update passes
+  bokehPass.renderTargetDepth.width = sizes.width * sizes.pixelRatio
+  bokehPass.renderTargetDepth.height = sizes.height * sizes.pixelRatio
 })
 
 /**
@@ -71,6 +84,7 @@ terrain.texture = {}
 terrain.texture.linesCount = 5
 terrain.texture.bigLineWidth = 0.04
 terrain.texture.smallLineWidth = 0.01
+terrain.texture.smallLineAlpha = 0.5
 terrain.texture.width = 32
 terrain.texture.height = 128
 terrain.texture.canvas = document.createElement('canvas')
@@ -103,7 +117,7 @@ terrain.texture.update = () => {
   const smallLinesCount = terrain.texture.linesCount - 1
 
   for (let i = 0; i < smallLinesCount; i++) {
-    terrain.texture.context.globalAlpha = 0.5
+    terrain.texture.context.globalAlpha = terrain.texture.smallLineAlpha
     terrain.texture.context.fillRect(
       0,
       actualBigLineWidth +
@@ -128,9 +142,37 @@ debug.Register({
   min: 1,
   max: 10,
   step: 1,
-  onChange: () => {
-    terrain.texture.update()
-  },
+  onChange: terrain.texture.update,
+})
+debug.Register({
+  object: terrain.texture,
+  label: 'bigLineWidth',
+  property: 'bigLineWidth',
+  type: 'range',
+  min: 0,
+  max: 0.1,
+  step: 0.0001,
+  onChange: terrain.texture.update,
+})
+debug.Register({
+  object: terrain.texture,
+  label: 'smallLineWidth',
+  property: 'smallLineWidth',
+  type: 'range',
+  min: 0,
+  max: 0.04,
+  step: 0.0001,
+  onChange: terrain.texture.update,
+})
+debug.Register({
+  object: terrain.texture,
+  label: 'smallLineAlpha',
+  property: 'smallLineAlpha',
+  type: 'range',
+  min: 0.1,
+  max: 1,
+  step: 0.0001,
+  onChange: terrain.texture.update,
 })
 
 // Geometry
@@ -174,13 +216,10 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 })
 renderer.setClearColor(0x111111, 1)
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Debug
-const guiDummy = {}
-guiDummy.clearColor = '#043136'
+const guiDummy = {
+  clearColor: '#043136',
+}
 debug.Register({
   object: guiDummy,
   label: 'clearColor',
@@ -190,6 +229,51 @@ debug.Register({
   onChange: () => {
     renderer.setClearColor(guiDummy.clearColor, 1)
   },
+})
+
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(sizes.pixelRatio)
+
+// Effect Composer
+const renderTarget = new THREE.WebGLMultisampleRenderTarget(800, 600, {
+  minFilter: THREE.LinearFilter,
+  magFilter: THREE.LinearFilter,
+  format: THREE.RGBAFormat,
+  encoding: THREE.sRGBEncoding,
+})
+const effectComposer = new EffectComposer(renderer)
+effectComposer.setSize(sizes.width, sizes.height)
+effectComposer.setPixelRatio(sizes.pixelRatio)
+
+// Render pass
+const renderPass = new RenderPass(scene, camera)
+effectComposer.addPass(renderPass)
+
+// Bokeh pass
+const bokehPass = new BokehPass(scene, camera, {
+  focus: 1.0,
+  aperture: 0.025,
+  maxblur: 0.01,
+  width: sizes.width * sizes.pixelRatio,
+  height: sizes.height * sizes.pixelRatio,
+})
+effectComposer.addPass(bokehPass)
+
+debug.Register({
+  type: 'folder',
+  label: 'bokehPass',
+  open: true,
+})
+debug.Register({
+  folder: 'bokehPass',
+  object: bokehPass.materialBokeh.uniforms.focus,
+  label: 'focus',
+  property: 'value',
+  type: 'range',
+  min: 0,
+  max: 10,
+  step: 0.01,
 })
 
 /**
@@ -207,7 +291,8 @@ const tick = () => {
   controls.update()
 
   // Render
-  renderer.render(scene, camera)
+  // renderer.render(scene, camera)
+  effectComposer.render()
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
