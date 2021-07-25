268c0b55b36b35ab8a1b0df1ec581778810af6e5
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-07-25 10:53:07 -0400

    add shaders

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import guify from 'guify'
+
+import terrainVertex from './shaders/terrain/vertex.glsl'
+import terrainFragment from './shaders/terrain/fragment.glsl'
 
 /**
  * Base
@@ -11,27 +15,35 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Debug
+
+const debug = new guify({
+  title: 'Terrain Controls',
+  align: 'right',
+  theme: 'dark',
+  barMode: 'none',
+})
+
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -49,21 +61,35 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /**
- * Cube
+ * Terrain
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(cube)
+
+const terrain = {}
+
+// Geometry
+terrain.geometry = new THREE.PlaneGeometry(1, 1, 100, 100)
+terrain.geometry.rotateX(-Math.PI * 0.5)
+
+// Material
+terrain.material = new THREE.ShaderMaterial({
+  vertexShader: terrainVertex,
+  fragmentShader: terrainFragment,
+})
+
+// Mesh
+terrain.mesh = new THREE.Mesh(terrain.geometry, terrain.material)
+terrain.mesh.scale.set(10, 10, 10)
+scene.add(terrain.mesh)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
+  canvas: canvas,
+  antialias: true,
 })
+renderer.setClearColor(0x111111, 1)
+renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -73,20 +99,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let lastElapsedTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - lastElapsedTime
-    lastElapsedTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - lastElapsedTime
+  lastElapsedTime = elapsedTime
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
